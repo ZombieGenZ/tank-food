3479d4b77a7d56cf65e0b0c90bb63cb2c30956c3
Nguyendinhnam28803 <Nguyendinhnam241209@gmail.com>
2025-03-07 16:53:10 +0700

    Tìm kiếm và cài đặt tool

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from '@mui/material';
+import { Skeleton } from '@mui/material';
 
 function App() {
   const [count, setCount] = useState(0)
+  const increase = () => {
+    setCount((counts) => counts + 1)
+  }
 
   return (
-    <>
+    <div className="w-full">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p className="bg-red-500">Giá trị: {count}</p>
+        <button onClick={increase}>Tăng</button>
+        <Button variant="contained">Text</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Skeleton/>
+    </div>
   )
 }
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { config } from 'dotenv'
 config()
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   server: {
     port: Number(process.env.APP_PORT) || 80
   }
