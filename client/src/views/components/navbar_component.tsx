b3d5cadaeb42d8b4f7b4ff8f1e456c89c2a713b3
Nguyendinhnam28803 <Nguyendinhnam241209@gmail.com>
2025-03-11 22:12:09 +0700

    tạo thanh nav

---
+++ client/src/views/components/navbar_component.tsx
@@ -0,0 +1,27 @@
+export const Navbar = [
+    {
+        id: 1,
+        title: "Trang chủ",
+        path: "/"
+    },
+    {
+        id: 2,
+        title: "Về TankFood",
+        path: "/aboutus"
+    },
+    {
+        id: 3,
+        title: "Thực đơn",
+        path: "/"
+    },
+    {
+        id: 4,
+        title: "Khuyến mãi",
+        path: "/"
+    },
+    {
+        id: 5,
+        title: "Liên hệ",
+        path: "/"
+    },
+];
--- client/src/views/pages/main.pages.tsx
+++ client/src/views/pages/main.pages.tsx
@@ -1,4 +1,6 @@
 import Loading from '../components/loading_page_components.tsx'
+import { Navbar } from '../components/navbar_component.tsx';
+import { IoFastFood } from "react-icons/io5";
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { useState, useEffect } from 'react'
 import Signup from './signup.pages.tsx';
@@ -21,6 +23,7 @@ const FormMain = () => {
         <div>
           <NavigationButtons />
           <Routes>
+            <Route path="/*" element={<Main />} />
             <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
@@ -33,10 +36,39 @@ function NavigationButtons() {
   const navigate = useNavigate();
 
   return (
-    <div className='flex justify-evenly'>
-      <button onClick={() => navigate("/signup")}>Đăng nhập</button>
+    <div className='sticky top-0 bg-[#FF6B35]'>
+      <div className="p-4 text-2xl flex justify-evenly">
+        {/* logo */}
+        <div className='flex items-center font-bold cursor-pointer'>
+          <p onClick={() => navigate("/")} className='text-[#ffffff] flex gap-2.5'>
+            <IoFastFood /> 
+            <p className='text-[#ffffff]'>Tank<span className='text-[#ffcc00]'>Food</span></p>
+          </p>
+        </div>
+        <div>
+          <ul className='flex items-center gap-10'>
+            {
+              Navbar.map((item) => {
+                return <li key={item.id} className='text-[#ffffff] cursor-pointer'>
+                        <button onClick={() => navigate(item.path)}>{item.title}</button> 
+                      </li>
+              })
+            }
+          </ul>
+        </div>
+        <div className='flex items-center cursor-pointer'>
+          <button className='text-[#ffffff]' onClick={() => navigate("/signup")}>Đăng nhập</button>
+        </div>
+      </div>
+      {/* <button onClick={() => navigate("/signup")}>Đăng nhập</button> */}
     </div>
   );
+}
+
+function Main() {
+  return (
+    <p>đây là trang chủ</p>
+  )
 }
 
 export default FormMain
