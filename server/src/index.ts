707b5f7859cccaeb5451b32aa7f15719837bd069
ZombieGenZ <zombiegenzzz@gmail.com>
2025-03-22 14:12:27 +0700

    Fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -47,7 +47,13 @@ app.use((req, res, next) => {
 })
 
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('trust proxy', true)
 
