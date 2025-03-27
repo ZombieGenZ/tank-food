000e7ff0073a584c27644290226816227f8d7e72
ZombieGenZ <zombiegenzzz@gmail.com>
2025-03-27 12:20:50 +0700

    Tối ưu hóa quá trình tự động unBan

--- server/src/jobs/functions/autoUnBanAccountExpiredBanned.functions.ts
+++ server/src/jobs/functions/autoUnBanAccountExpiredBanned.functions.ts
@@ -9,28 +9,41 @@ export const autoUnBanAccountExpiredBanned = async () => {
   const currentDate = new Date()
   const userBanned = await databaseService.users.find({ penalty: { $ne: null } }).toArray()
 
+  const promises = [] as Promise<void>[]
+
   for (const user of userBanned) {
     if (user.penalty == null) {
       continue
     }
 
     if (user.penalty.expired_at > currentDate) {
-      AccountmManagementService.unBan(user._id.toString())
-      if (serverLanguage == LANGUAGE.VIETNAMESE) {
-        console.log(
-          `\x1b[33mĐã tự động mở khóa tài khoản \x1b[36m${user.display_name}\x1b[33m (ID: \x1b[36m${user._id}\x1b[33m) thành công\x1b[0m`
-        )
-        await writeInfoLog(
-          `Đã tự động mở khóa tài khoản ${user.display_name} (ID: ${user._id}) thành công (Time: ${formatDateFull2(currentDate)})`
-        )
-      } else {
-        console.log(
-          `\x1b[33mSuccessfully automatically unlocked account \x1b[36m${user.display_name}\x1b[33m (ID: \x1b[36m${user._id}\x1b[33m)\x1b[0m`
-        )
-        await writeInfoLog(
-          `Successfully automatically unlocked account ${user.display_name} (ID: ${user._id}) (Time: ${formatDateFull2(currentDate)})`
-        )
-      }
+      promises.push(
+        new Promise((resolve, reject) => {
+          try {
+            AccountmManagementService.unBan(user._id.toString())
+            if (serverLanguage == LANGUAGE.VIETNAMESE) {
+              console.log(
+                `\x1b[33mĐã tự động mở khóa tài khoản \x1b[36m${user.display_name}\x1b[33m (ID: \x1b[36m${user._id}\x1b[33m) thành công\x1b[0m`
+              )
+              writeInfoLog(
+                `Đã tự động mở khóa tài khoản ${user.display_name} (ID: ${user._id}) thành công (Time: ${formatDateFull2(currentDate)})`
+              )
+            } else {
+              console.log(
+                `\x1b[33mSuccessfully automatically unlocked account \x1b[36m${user.display_name}\x1b[33m (ID: \x1b[36m${user._id}\x1b[33m)\x1b[0m`
+              )
+              writeInfoLog(
+                `Successfully automatically unlocked account ${user.display_name} (ID: ${user._id}) (Time: ${formatDateFull2(currentDate)})`
+              )
+            }
+            resolve()
+          } catch (err) {
+            reject()
+          }
+        })
+      )
     }
   }
+
+  await Promise.all(promises)
 }
