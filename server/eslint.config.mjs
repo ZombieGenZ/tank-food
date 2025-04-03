a6392385592015b0f23951bf87a0dec84a96ab8e
ZombieGenZ <zombiegenzzz@gmail.com>
2025-04-03 16:43:30 +0700

    Update ESLint configuration to enhance TypeScript and React support

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,23 +1,35 @@
+import js from '@eslint/js'
 import globals from 'globals'
-import pluginJs from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ['**/*.{js,mjs,cjs,ts}']
+    files: ['**/*.{ts,tsx}']
   },
   {
-    languageOptions: { globals: globals.browser }
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    }
   },
-  pluginJs.configs.recommended,
+  js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
       prettier: eslintPluginPrettier
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
+      ],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'prettier/prettier': [
@@ -35,6 +47,6 @@ export default [
         }
       ]
     },
-    ignores: ['**/node_modules/', '**/dist/']
+    ignores: ['dist']
   }
 ]
